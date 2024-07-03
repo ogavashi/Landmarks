8a6452b324ea1dd78a63debc49a155d080d24396
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2024-07-03 23:13:58 +0300

    add base logic

--- Landmarks/Views/Categories/CategoryItem.swift
+++ Landmarks/Views/Categories/CategoryItem.swift
@@ -5,4 +5,29 @@
 //  Created by Oleg Gavashi on 03.07.2024.
 //
 
-import Foundation
+import SwiftUI
+
+
+struct CategoryItem: View {
+    var landmark: Landmark
+    
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .renderingMode(.original)
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .foregroundStyle(.primary)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+
+#Preview {
+    CategoryItem(landmark: ModelData().landmarks[0])
+}
--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -8,36 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: Tab = .featured
+    
+    enum Tab {
+        case featured
+        case list
+    }
+    
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300)
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                HStack{
-                    Text("Joshua Tree National Park")
-                    Spacer()
-                    Text("California")
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
                 }
-                .font(.subheadline)
-                .foregroundStyle(.secondary)
-                Divider()
-                
-                
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
-            }
-            .padding()
-            Spacer()
+                .tag(Tab.featured)
+            
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
         }
     }
 }
 
 #Preview {
     ContentView()
+        .environment(ModelData())
 }
